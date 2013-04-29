76eabe930818e5c111246b637a98a5811e8067da
DariusL <d.lapunas@gmail.com>
2013-04-29 22:00:38 +0300

    resolve done

--- darlap1_resolv02.c
+++ darlap1_resolv02.c
@@ -2,17 +2,14 @@
 /*darlap1_resolv02.c*/
 
 #include <aio.h>
-#include <unistd.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <stdlib.h>
-#include <sys/resource.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <string.h>
 
 void dl_resolve(char *name){
 	struct addrinfo hints, *result, *rp;
@@ -21,13 +18,13 @@ void dl_resolve(char *name){
 	length = sysconf(_SC_HOST_NAME_MAX);
 	buf = malloc(length);
 	memset((void*)&hints, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_flags = AI_ALL|AI_V4MAPPED;
+	hints.ai_family = PF_INET;
+	hints.ai_flags = AI_V4MAPPED;
 	if(getaddrinfo(name, NULL, &hints, &result) != 0){
 		printf("getaddrinfo sprogo\n");
 		exit(1);
 	}
-	for(rp = result, rp != NULL; rp = rp->ai_next){
+	for(rp = result; rp != NULL; rp = rp->ai_next){
 		res = getnameinfo(rp->ai_addr, rp->ai_addrlen, buf, length, NULL, NULL, NI_NUMERICHOST);
 		if(res == 0)
 			printf("IP: %s\n", buf);
